ec72ed407ecbdd348c2113351399199632f00c43
Om Bhosale <67160092+ombhosale2510@users.noreply.github.com>
2025-01-11 14:43:42 -0500

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,27 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-    'jest/globals': true
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
-    },
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  'plugins': [
-    'react', 'jest'
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
-    'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
-    'no-console': 'error',
-    'react/prop-types': 0
-  }
-};
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"],
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+  },
+}
