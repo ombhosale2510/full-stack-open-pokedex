22ad77a50ee5f832d37f746b554d15032f07abaf
Om Bhosale <67160092+ombhosale2510@users.noreply.github.com>
2025-01-11 15:15:28 -0500

    update try

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true, // Add this line
+    jest: true,
   },
   "extends": [
     "eslint:recommended",
